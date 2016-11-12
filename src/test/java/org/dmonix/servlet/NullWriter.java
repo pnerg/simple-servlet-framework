9b3a667f55684f26cb11b4d202c84bc6814db045
Peter Nerg <peter.nerg@gmail.com>
2016-11-12 13:17:32 +0100

    added NullWriter

---
+++ src/test/java/org/dmonix/servlet/NullWriter.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2016 Peter Nerg
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dmonix.servlet;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Writer that does nothing.
+ * @author Peter Nerg
+ */
+final class NullWriter extends Writer {
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+    }
+
+    @Override
+    public void flush() throws IOException {
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+}
