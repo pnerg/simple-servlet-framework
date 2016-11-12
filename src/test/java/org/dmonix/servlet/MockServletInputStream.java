6a74b0cbfe11be353978a3b6c7ea2c9c33134194
Peter Nerg <peter.nerg@gmail.com>
2016-11-12 08:54:35 +0100

    added MockServletInputStream

---
+++ src/test/java/org/dmonix/servlet/MockServletInputStream.java
@@ -0,0 +1,60 @@
+package org.dmonix.servlet;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Mocke implementation of a {@link javax.servlet.ServletInputStream} used to provide JSon response data
+ * @author Peter Nerg
+ */
+public final class MockServletInputStream extends ServletInputStream {
+    private final InputStream wrapped;
+
+    /**
+     * Creates the stream with a JSon string.
+     * @param response
+     */
+    public MockServletInputStream(String response) {
+        this.wrapped = new ByteArrayInputStream(response.getBytes());
+    }
+
+    /**
+     * Unsupported operation.
+     * @return
+     */
+    @Override
+    public boolean isFinished() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Unsupported operation.
+     * @return
+     */
+    @Override
+    public boolean isReady() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Unsupported operation.
+     * @return
+     */
+    @Override
+    public void setReadListener(ReadListener readListener) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Delegates the read to the wrapped {@link ByteArrayInputStream} containing the JSon string data.
+     * @return
+     * @throws IOException
+     */
+    @Override
+    public int read() throws IOException {
+        return wrapped.read();
+    }
+}
